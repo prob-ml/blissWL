ac3bb7a22d82e944ec8a3dd84f54ba458988902f
Tim White <twhit@umich.edu>
2026-01-29 18:17:36 -0500

    Remove descwl-shear-sims from venv, borrow necessary code without lsst_stack

--- images_to_maps/descwl/run_anacal.py
+++ images_to_maps/descwl/run_anacal.py
@@ -28,10 +28,6 @@ from numpy.random import SeedSequence
 from omegaconf import DictConfig
 from tqdm import tqdm
 
-from descwl_shear_sims.psfs import make_ps_psf
-from descwl_shear_sims.sim import get_se_dim
-
-
 # =============================================================================
 # AnaCal Utility Classes
 # =============================================================================
@@ -58,6 +54,179 @@ class GalSimPsfWrapper(anacal.psf.BasePsf):
         return psf_at_pos.drawImage(
             nx=self.npix, ny=self.npix, scale=self.pixel_scale, method="auto"
         ).array.astype(np.float64)
+
+
+# =============================================================================
+# PSF Functions (copied from descwl-shear-sims to avoid LSST stack dependency)
+# =============================================================================
+
+SCALE = 0.2  # pixel scale in arcsec/pixel
+
+
+def get_se_dim(*, coadd_dim, coadd_scale=None, se_scale=None, rotate=False):
+    """
+    Get single epoch (se) dimensions given coadd dim.
+    Copied from descwl-shear-sims/descwl_shear_sims/sim.py
+    """
+    if (coadd_scale is None) or (se_scale is None):
+        dim = coadd_dim
+    else:
+        coadd_length = coadd_scale * coadd_dim
+        dim = int((coadd_length) / se_scale + 0.5)
+    if rotate:
+        se_dim = int(np.ceil(dim * np.sqrt(2))) + 20
+    else:
+        se_dim = dim + 10
+    return se_dim
+
+
+class PowerSpectrumPSF:
+    """
+    Spatially varying Moffat PSF according to Heymans et al. (2012).
+    Copied from descwl-shear-sims/descwl_shear_sims/psfs/ps_psf.py
+    """
+
+    def __init__(
+        self,
+        *,
+        rng,
+        im_width,
+        buff,
+        scale,
+        trunc=1,
+        noise_level=None,
+        variation_factor=1,
+        median_seeing=0.8,
+    ):
+        self._rng = rng
+        self._im_cen = (im_width - 1) / 2
+        self._scale = scale
+        self._tot_width = im_width + 2 * buff
+        self._x_scale = 2.0 / self._tot_width / scale
+        self._noise_level = noise_level
+        self._buff = buff
+        self._variation_factor = variation_factor
+        self._median_seeing = median_seeing
+
+        def _pf(k):
+            return (k**2 + (1.0 / 180) ** 2) ** (-11.0 / 6.0) * np.exp(
+                -((k * trunc) ** 2)
+            )
+
+        self._ps = galsim.PowerSpectrum(e_power_function=_pf, b_power_function=_pf)
+        ng = 128
+        gs = max(self._tot_width * self._scale / ng, 1)
+        self.ng = ng
+        self.gs = gs
+        seed = self._rng.randint(1, 2**30)
+        self._ps.buildGrid(
+            grid_spacing=gs,
+            ngrid=ng,
+            get_convergence=True,
+            variance=(0.01 * variation_factor) ** 2,
+            rng=galsim.BaseDeviate(seed),
+        )
+
+        g1_grid, g2_grid, mu_grid = galsim.lensing_ps.theoryToObserved(
+            self._ps.im_g1.array, self._ps.im_g2.array, self._ps.im_kappa.array
+        )
+
+        self._lut_g1 = galsim.table.LookupTable2D(
+            self._ps.x_grid,
+            self._ps.y_grid,
+            g1_grid.T,
+            edge_mode="wrap",
+            interpolant=galsim.Lanczos(5),
+        )
+        self._lut_g2 = galsim.table.LookupTable2D(
+            self._ps.x_grid,
+            self._ps.y_grid,
+            g2_grid.T,
+            edge_mode="wrap",
+            interpolant=galsim.Lanczos(5),
+        )
+        self._lut_mu = galsim.table.LookupTable2D(
+            self._ps.x_grid,
+            self._ps.y_grid,
+            mu_grid.T - 1,
+            edge_mode="wrap",
+            interpolant=galsim.Lanczos(5),
+        )
+
+        self._g1_mean = self._rng.normal() * 0.01 * variation_factor
+        self._g2_mean = self._rng.normal() * 0.01 * variation_factor
+
+        if self._noise_level is not None and self._noise_level > 0:
+            self._noise_field = (
+                self._rng.normal(size=(im_width + buff + 37, im_width + buff + 37))
+                * noise_level
+            )
+
+        def _getlogmnsigma(mean, sigma):
+            logmean = np.log(mean) - 0.5 * np.log(1 + sigma**2 / mean**2)
+            logvar = np.log(1 + sigma**2 / mean**2)
+            logsigma = np.sqrt(logvar)
+            return logmean, logsigma
+
+        lm, ls = _getlogmnsigma(self._median_seeing, 0.1)
+        self._fwhm_central = np.exp(self._rng.normal() * ls + lm)
+
+    def _get_lensing(self, pos):
+        pos_x, pos_y = galsim.utilities._convertPositions(
+            pos, galsim.arcsec, "_get_lensing"
+        )
+        return (
+            self._lut_g1(pos_x, pos_y),
+            self._lut_g2(pos_x, pos_y),
+            self._lut_mu(pos_x, pos_y) + 1,
+        )
+
+    def _get_atm(self, x, y):
+        xs = (x + 1 - self._im_cen) * self._scale
+        ys = (y + 1 - self._im_cen) * self._scale
+        g1, g2, mu = self._get_lensing((xs, ys))
+
+        if g1 * g1 + g2 * g2 >= 1.0:
+            norm = np.sqrt(g1 * g1 + g2 * g2) / 0.5
+            g1 /= norm
+            g2 /= norm
+
+        fwhm = self._fwhm_central / np.power(mu, 0.75)
+        psf = galsim.Moffat(beta=2.5, fwhm=fwhm).shear(
+            g1=g1 + self._g1_mean, g2=g2 + self._g2_mean
+        )
+        return psf
+
+    def getPSF(self, pos):
+        """Get PSF model at position (zero-indexed pixel coordinates)."""
+        psf = self._get_atm(pos.x, pos.y)
+
+        if self._noise_level is not None and self._noise_level > 0:
+            xll = int(pos.x + self._buff - 16)
+            yll = int(pos.y + self._buff - 16)
+            assert xll >= 0 and xll + 33 <= self._noise_field.shape[1]
+            assert yll >= 0 and yll + 33 <= self._noise_field.shape[0]
+
+            stamp = self._noise_field[yll : yll + 33, xll : xll + 33].copy()
+            psf += galsim.InterpolatedImage(
+                galsim.ImageD(stamp, scale=self._scale), normalization="sb"
+            )
+
+        return psf.withFlux(1.0)
+
+
+def make_ps_psf(*, rng, dim, pixel_scale=SCALE, variation_factor=1):
+    """
+    Create a power spectrum PSF.
+    Copied from descwl-shear-sims/descwl_shear_sims/psfs/ps_psf.py
+    """
+    return PowerSpectrumPSF(
+        rng=rng,
+        im_width=dim,
+        buff=dim / 2,
+        scale=pixel_scale,
+        variation_factor=variation_factor,
+    )
 
 
 # =============================================================================
